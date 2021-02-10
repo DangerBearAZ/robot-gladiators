af4daee307e0268e5da985c5722d30ca604a8026
claire ogesen <claire.ogesen.co@gmail.com>
2021-02-10 00:37:59 -0700

    adding rounds

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,11 +4,6 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-
-console.log(playerName);
-console.log(playerHealth, playerAttack, playerMoney);
-console.log(enemyNames);
-
 var enemyNames = ["BBAG", "TYLER", "ODIN"];
 var enemyHealth = 50;
 var enemyAttack = 12;
@@ -24,10 +19,9 @@ console.log(enemyNames[i] + " is at " + i + "index");
 
 // fight function
 var fight = function(enemyNames) {
-  // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
-
-  // ask player if they'd like to fight or run
+  while(enemyHealth > 0) {
+  
+   // fight or skip 
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
   // if player choses to fight, fight
@@ -76,9 +70,14 @@ var fight = function(enemyNames) {
   } else {
     window.alert("You need to pick a valid option. Try again!");
   }
-  };
+  }
+};
       
 // run fight function to start game
-for (var i = 0; i < enemyNames.length; i++) {
-  fight(enemyNames[i]);
+for(var i = 0; i < enemyNames.length; i++) {
+ var pickedEnemyName = enemyNames [i];
+ enemyHealth = 50;
+fight(pickedEnemyName);
 }
+
+//fight();
