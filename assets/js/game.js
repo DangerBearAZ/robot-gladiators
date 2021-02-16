67a0915c54f8fb1fe6a1fb7ab6960b3dcc2af325
claire ogesen <claire.ogesen.co@gmail.com>
2021-02-15 21:54:54 -0700

    updated

--- assets/js/game.js
+++ assets/js/game.js
@@ -75,7 +75,6 @@ var endGame = function () {
 var fight = function (enemy) {
   // keep track of who goes first
   var isPlayerTurn = true;
-
   // randomly change turn order
   if (Math.random() > 0.5) {
     isPlayerTurn = false;
