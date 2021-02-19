51da2f0c80636c361d58488d9e157473d88d1d3c
claire ogesen <claire.ogesen.co@gmail.com>
2021-02-18 19:32:38 -0700

    aaannndddd done

--- assets/js/game.js
+++ assets/js/game.js
@@ -51,13 +51,22 @@ var startGame = function() {
 var endGame = function () {
   window.alert("The game has now ended. Let's see how you did!");
 
-  // if player is still alive, player wins!
-  if (playerInfo.health > 0) {
-    window.alert("Great job, you've survived the game! You now have a score of" + playerInfo.money + '.');
-  } else {
-    window.alert("You've lost your robot in battle!");
+  //Check localStorage for high score, if it's not there, use 0
+  var highSccore = localStorage.getItem("highscore");
+  if (hightScore === null) {
+    highSccore = 0;
   }
 
+  //if player has more money than the high score,player has new high scre
+  if (playerInfo.money > hightScore) {
+    localStorage.setItem("highscore", playerInfo.money);
+    localStorage.setItem("name", PlayerInfo.name);
+
+    alert(playerInfo.name + " now has the high score of " + playerInfo.money)
+  }
+  else {
+    alert (alayerIno.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+  }
 
   // ask player if they'd like to play again
   var playAgainConfirm = window.confirm('Would you like to play again?');
